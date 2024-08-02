836a778c7bafd458b497da2529f60049ec4c2304
Joe Wright <joewright87@gmail.com>
2024-08-02 13:36:30 -0400

    add a scene with some sound effects

--- jw_demo/app.py
+++ jw_demo/app.py
@@ -19,9 +19,10 @@ class App:
     current_scene: SceneMeta = None
     menu = [
         # x, y, text, color (aligns with pyxel.text method args)
-        (20, 2, "Scene A", pyxel.COLOR_CYAN),
-        (60, 2, "Ok", pyxel.COLOR_LIME),
-        (72, 2, "X", pyxel.COLOR_ORANGE),
+        (2, 2, "Scene A", pyxel.COLOR_CYAN),
+        (32, 2, "Scene B", pyxel.COLOR_LIME),
+        (64, 2, "Scene 3", pyxel.COLOR_PURPLE),
+        (96, 2, "X", pyxel.COLOR_ORANGE),
     ]
     show_exit_message = False
 
@@ -29,7 +30,7 @@ class App:
         self.pyxel = pyxel
         pyxel.init(120, 120, title="We done did it", fps=self.target_fps)
         pyxel.mouse(True)
-        self.scenes = [SceneA(self), SceneB(self)]
+        self.scenes = [SceneA(self), SceneB(self), SceneC(self)]
         pyxel.run(self.update, self.draw)
 
     def detect_menu_click(self):
@@ -150,6 +151,86 @@ class SceneB(SceneMeta):
 
     def draw(self):
         self.app.pyxel.text(10, 10, self.text, pyxel.COLOR_RED)
+
+
+class SoundController:
+    images = [
+        # sprite sheet X, sprite sheet Y, width, height
+        # will be passed to pyxel.blt as args: u, v, w, h
+        # sheet is shared with the Dancer class above
+        # up, up right, right, down right, down, down left, left, up left
+        (0, 16, 8, 8),
+        (8, 16, 8, 8),
+        (0, 24, 8, 8),
+        (8, 24, 8, 8),
+        (0, 32, 8, 8),
+        (8, 32, 8, 8),
+        (0, 40, 8, 8),
+        (8, 40, 8, 8),
+    ]
+    image_positions = [
+        # draw the images clockwise starting from up, each an 8x8 square
+        (0, 0),
+        (8, 8),
+        (16, 16),
+        (8, 24),
+        (0, 32),
+        (-8, 24),
+        (-16, 16),
+        (-8, 8),
+    ]
+    overall_xy = [32, 24]
+    sound_notes = ["C#2", "D2", "E2", "F2", "G2", "A2", "B2", "C2"]
+    notes = list()
+
+    def __init__(self, app: App):
+        self.app = app
+        for note in self.sound_notes:
+            sound = self.app.pyxel.Sound()
+            sound.set_notes(note)
+            self.notes.append(sound)
+
+    def handle_click(self):
+        # check if the mouse is within the bounds of the controller
+        bounds = [0, 0, 0, 0]
+        for index, position in enumerate(self.image_positions):
+            x, y = position
+            x += self.overall_xy[0]
+            y += self.overall_xy[1]
+            bounds[0] = x
+            bounds[1] = y
+            bounds[2] = x + 8
+            bounds[3] = y + 8
+            if (
+                bounds[0] <= pyxel.mouse_x <= bounds[2]
+                and bounds[1] <= pyxel.mouse_y <= bounds[3]
+            ):
+                self.app.pyxel.play(snd=self.notes[index], ch=0, loop=False)
+
+    def draw(self):
+        for index, image in enumerate(self.images):
+            x, y = self.image_positions[index]
+            x += self.overall_xy[0]
+            y += self.overall_xy[1]
+            self.app.pyxel.blt(x, y, 0, *image)
+
+
+class SceneC(SceneMeta):
+
+    def __init__(self, app: App):
+        self.app = app
+        self.text = "Push the buttons!"
+        # we should load this in the main app if we're using it across scenes
+        app.pyxel.load("assets/characters.pyxres")
+        self.controller = SoundController(app)
+
+    def update(self):
+        if self.app.pyxel.btn(pyxel.MOUSE_BUTTON_LEFT):
+            self.controller.handle_click()
+
+    def draw(self):
+        self.app.pyxel.text(10, 10, self.text, pyxel.COLOR_RED)
+        self.controller.draw()
 
 
 # run the app
